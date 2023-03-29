5adb5972c4a4eaf67dfc87a2e9b6a0e18821378f
yannelly25 <yannellym@gmail.com>
2023-03-28 22:00:12 -0400

    part 4 completed

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,6 +3,8 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -32,26 +34,30 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // Call helper method to swap the FrameLayout with the fragment
-        replaceFragment(ArticleListFragment())
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val bestSellerBooksFragment: Fragment = BestSellerBooksFragment()
+        val articleListFragment: Fragment = ArticleListFragment()
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
+        // handle navigation selection
         bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
             when (item.itemId) {
-                R.id.action_favorites ->
-                    // do something here
-                    true
-                R.id.action_schedules ->
-                    // do something here
-                    true
-                else -> true
+                R.id.action_books -> fragment = bestSellerBooksFragment
+                R.id.action_articles -> fragment = articleListFragment
             }
+            replaceFragment(fragment)
+            true
         }
 
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.action_books
     }
     //This FrameLayout will be where the fragment will be dynamically placed at runtime.
-    private fun replaceFragment(articleListFragment: ArticleListFragment) {
+    private fun replaceFragment(articleListFragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.article_frame_layout, articleListFragment)
